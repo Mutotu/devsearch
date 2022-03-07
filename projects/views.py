2a3a82eb0546a242b5fe170d2bba5364ce2fb98c
mutotu <muttalip.aydemir@gmail.com>
2022-03-07 14:19:50 +0000

    communication between html and django

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,41 @@
+from email.mime import message
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
 
 
 def projects(request):
     # return HttpResponse('Here are the products')
-    return render(request, 'projects/projects.html')
+    #####3
+    # page = 'projects'
+    # return render(request, 'projects/projects.html', {'page' : page} )
+    ######
+    page = 'projects'
+    number= 101
+    context = {'page':page, 'number':number,'projects':projectsList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     # return HttpResponse('Here is the product')
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
